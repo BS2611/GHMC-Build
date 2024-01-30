356868b2c331cfd4f899521c2d1d0d3baa02023d
BS2611 <bhavpreetsingh93@gmail.com>
2024-01-29 17:36:53 -0800

    Reviews Dynamic

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,39 +1,10 @@
 import React from "react";
 
 import Slider from "react-slick";
-import { getDarkBg, getLightBg } from "../config/config";
+import { getDarkBg, getJsonObject, getLightBg } from "../config/config";
 
 const Testimonials = ({ classicHeader, darkTheme, compNo }) => {
-  const reviews = [
-    {
-      name: "Dennis Jacques",
-      position: "User from USA",
-      src: "images/testimonial/client-sm-3.jpg",
-      desc: "Only trying it out since a few days. But up to now excellent. Seems to work flawlessly. priced simply dummy text of the printing and typesetting industry.",
-      rating: 5,
-    },
-    {
-      name: "Jay Shah",
-      position: "Founder at Icomatic Pvt Ltd",
-      src: "images/testimonial/client-sm-1.jpg",
-      desc: "Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure.",
-      rating: 5,
-    },
-    {
-      name: "Patrick Cary",
-      position: "Freelancer from USA",
-      src: "images/testimonial/client-sm-2.jpg",
-      desc: "I am happy Working with printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam persequeris essent possim iriure.",
-      rating: 5,
-    },
-    {
-      name: "Chris Tom",
-      position: "User from UK",
-      src: "images/testimonial/client-sm-4.jpg",
-      desc: "I have used them twice now. Good rates, very efficient service and priced simply dummy text of the printing and typesetting industry quidam interesset his et. Excellent.",
-      rating: 5,
-    },
-  ];
+  const reviews = getJsonObject("reviews");
 
   var settings = {
     dots: true,
